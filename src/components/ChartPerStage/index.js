87fd03fc6ffbee205a32595d79eb45624d184553
ThieenJ <94862916+thieenjdev03@users.noreply.github.com>
2024-07-13 21:19:11 +0700

    Update code

---
+++ src/components/ChartPerStage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChartPerStage(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ChartPerStage;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     return (
         <>
             <div className='header flex items-center bg-black w-full text-white p-4 gap-10 justify-center'>
-                <a href='/#'>Home</a>
+                <a href='/'>Home</a>
                 <a href='/ChartPerTeamPage'>Thống kê theo đội bóng</a>
                 <a href='/ChartPerStage'>Thống kê theo vòng</a>
                 <a href='/ChartPerGroup'>Thống kê theo bảng</a>
